7a154539a64bb2f019882bf33415689f02af42e8
aiden <65510342+b1gbrain@users.noreply.github.com>
2020-07-03 19:19:00 -0400

    Update idfk.lua

--- idfk.lua
+++ idfk.lua
@@ -3,21 +3,21 @@ local script = file:read()
 file:close()
 
 --("").char(97) --> a
-function obfuscate(contents)
+function gamer(contents)
     local Chars = {}
-    local obfuscatedScript = "_={}_[#_+1]=('').char;___=loadstring;____=table.concat;_____={"
+    local ret = "_={}_[#_+1]=('').char;___=loadstring;____=table.concat;_____={"
 for i=1,string.len(contents) do
         Chars[i] = string.byte(contents,i)
     end
     
     for i,v in pairs(Chars) do
-        obfuscatedScript = obfuscatedScript.."_[#_]("..v..")"..","
+        ret = ret.."_[#_]("..v..")"..","
     end
-    obfuscatedScript = obfuscatedScript:sub(1,string.len(obfuscatedScript)-1).."}"
-    obfuscatedScript = obfuscatedScript.."___(____(_____))()"
+    ret = ret:sub(1,string.len(ret)-1).."}"
+    ret = ret.."___(____(_____))()"
     
 
-    return obfuscatedScript
+    return ret
 end
 
-print(obfuscate(script))
+print(gamer(script))
